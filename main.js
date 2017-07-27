d5b5f53ee6f89f3ce37e09d1775962f479ce1068
AaronKWalker <aaron.walker@outlook.com>
2017-07-27 17:13:23 -0500

    more modifications

--- main.js
+++ main.js
@@ -4,86 +4,84 @@ var inquirer = require("inquirer");
 
 var historyDeck = [
   {
-    compText:"",
-    missingBit:""
+    compText:"When Nazi Germany planned to invade the UK during WWII, Hitler name the planned invasion Operation Sealion",
+    missingBit:"Operation Sealion"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"During the first times of exploration, the Western Hemisphere, in particular the Americas, were known as The New World",
+    missingBit:"New World"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"	Abraham Lincoln, a US President, gave the famous Gettysburg Address Speech",
+    missingBit:"Abraham Lincoln"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"The War of the Grand Alliance was also known as the Nine Years' War",
+    missingBit:"Nine Years'"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"In 1938, the Anderson Shelter was designed from corrugated steel by William Paterson and Carl Kernson",
+    missingBit:"Anderson Shelter"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"The First and Second Boer War was the name given to the South African Wars of 1880-1881 and 1899-1902",
+    missingBit:"First and Second Boer War"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"6th June, 1944 was the date of D-Day",
+    missingBit:"6th June, 1944"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"Hadrian's Wall is a famous wall, built by a Roman Emperor, that lies between Scotland and England",
+    missingBit:"Hadrian's Wall"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"Dallas, Texas is the name of the city where John F. Kennedy was assassinated",
+    missingBit:"Dallas, Texas"
   }
 ];
 
 var scienceDeck = [
   {
-    compText:"",
-    missingBit:""
+    compText:"Albert Einstein created the famous equation E = mc2",
+    missingBit:"Albert Einstein"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"Sir Isaac Newton discovered the law of gravity",
+    missingBit:"Sir Isaac Newton"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"There are 206 bones in the human body",
+    missingBit:"206"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"The letters DNA stand for",
+    missingBit:"Deoxyribonucleic acid"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"The chemical element Helium has the symbol He",
+    missingBit:"Helium"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"Fe is the Chemical Symbol for Iron",
+    missingBit:"Fe"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"Mercury is the only metal that is liquid at room temperature",
+    missingBit:"Mercury"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"Thomas Edison invented the gramophone",
+    missingBit:"Thomas Edison"
   },
   {
-    compText:"",
-    missingBit:""
+    compText:"Sunlight provides Vitamin D to humans",
+    missingBit:"D"
   }
 ];
 
 function quizMe(){
   inquirer.prompt([
-
-  ]).then(function(inquirerResponse){
     {
       type: "input",
       message: "What is your name?",
@@ -93,7 +91,21 @@ function quizMe(){
       type: "list",
       message: "Which would you like to be quized on?",
       choices: ["history", "science"],
-      name: "pokemon"
+      name: "userChoice"
+    }
+  ]).then(function(inquirerResponse.userChoice === "history"){
+    if (inquirerResponse.){
+      historyQ();
+    } else {
+      scienceQ();
     }
   });//-end of .then-
 }//-end of quizMe-
+
+function historyQ(){
+
+}//-end of historyQ-
+
+function scienceQ(){
+
+}//-end of scienceQ-
